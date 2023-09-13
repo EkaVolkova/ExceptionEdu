35a87665ca3d95319a5f55515770c7352eb0c248
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-13 21:33:32 +0500

    9.1.4 Создайте класс исключения Exception и переопределите его свойство Message, а также свойство HelpLink, добавив в него ссылку на внешний ресурс.

--- ExceptionEdu/ExceptionEdu/Program.cs
+++ ExceptionEdu/ExceptionEdu/Program.cs
@@ -2,6 +2,8 @@
 
 /// <summary>
 /// 9.1.3 Создайте класс исключения Exception и добавьте в свойство Data дату создания исключения.
+/// 9.1.4Создайте класс исключения Exception и переопределите его свойство Message, 
+/// а также свойство HelpLink, добавив в него ссылку на внешний ресурс.
 /// </summary>
 namespace ExceptionEdu
 {
@@ -9,8 +11,9 @@ namespace ExceptionEdu
     {
         static void Main(string[] args)
         {
-            Exception exception = new Exception();
+            Exception exception = new Exception("Исключение!");
             exception.Data.Add("Дата создания исключения: ", DateTime.Now);
+            exception.HelpLink = @"https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/";
         }
     }
 }
