580849d99ddce263fb5ed1317aff8dcaabaf808c
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-13 21:25:33 +0500

    9.1.3 Создайте класс исключения Exception и добавьте в свойство Data дату создания исключения.

---
+++ ExceptionEdu/ExceptionEdu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// 9.1.3 Создайте класс исключения Exception и добавьте в свойство Data дату создания исключения.
+/// </summary>
+namespace ExceptionEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception();
+            exception.Data.Add("Дата создания исключения: ", DateTime.Now);
+        }
+    }
+}
