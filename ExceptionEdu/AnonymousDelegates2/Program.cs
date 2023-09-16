7d3c17a41e3267e7a43ddd9f9e170fa072156def
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 18:15:37 +0500

    9.3.13 Существует консольное решение, которое выводит случайное целое число в диапазоне от 0 до 100 и отображает результат в консольное сообщение:
    class Program
        {
            delegate int RandomNumberDelegate();
            static void Main(string[] args)
            {
                RandomNumberDelegate randomNumberDelegate = RandomNumber;
                int result = randomNumberDelegate.Invoke();
                Console.WriteLine(result);
                Console.Read();
            }
    
            static int RandomNumber()
            {
                return new Random().Next(0, 100);
            }
        }
    Реализуйте в данном решении анонимный метод, не сломав логику приложения.

---
+++ ExceptionEdu/AnonymousDelegates2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Существует консольное решение, которое выводит случайное целое число в диапазоне от 0 до 100 и отображает результат в консольное сообщение:
+///     class Program
+///     {
+///         delegate int RandomNumberDelegate();
+///         static void Main(string[] args)
+///         {
+///             RandomNumberDelegate randomNumberDelegate = RandomNumber;
+///             int result = randomNumberDelegate.Invoke();
+///             Console.WriteLine(result);
+///             Console.Read();
+///         }
+///         
+///         static int RandomNumber()
+///         {
+///             return new Random().Next(0, 100);
+///         }         
+///     }
+///     
+/// Реализуйте в данном решении анонимный метод, не сломав логику приложения.
+/// </summary>
+namespace AnonymousDelegates2
+{
+    class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = delegate()
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+    }
+}
