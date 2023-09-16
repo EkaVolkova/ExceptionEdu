d61f83f4f4879b4a1ec853889bcbb1883e84c6a7
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 18:40:00 +0500

    9.4.2 Cоздайте консольное приложение, в котором реализуйте демонстрацию ковариантности делегатов при помощи следующей модели классов:
    class Car {}
    class Lexus: Car {}

---
+++ ExceptionEdu/Covariance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Создайте консольное приложение, в котором реализуйте демонстрацию ковариантности делегатов при помощи следующей модели классов:
+/// class Car {}
+/// class Lexus : Car { }
+/// </summary>
+namespace Covariance
+{
+    class Program
+    {
+        delegate Car Handler();
+        static void Main(string[] args)
+        {
+            Handler handlerCar = HandlerCar;
+            Handler handlerLexus = HandlerLexus;
+        }
+        static Car HandlerCar()
+        {
+            return new Car();
+        }
+        static Lexus HandlerLexus()
+        {
+            return new Lexus();
+        }
+
+    }
+    class Car { }
+    class Lexus : Car { }
+
+}
