6b3f4cb25a1fd41227ced2ba971205cb3c00bdf8
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 17:39:27 +0500

    9.3.2 Создайте консольное приложение, в котором есть функция, принимающая на вход два числа int, и возвращающая результат int вычитания второго числа из первого. Вызовите эту функцию в классе Main при помощи делегата и отобразите результат в консольном сообщении.

---
+++ ExceptionEdu/Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// 9.3.2 Создайте консольное приложение, в котором есть функция, принимающая на вход два числа int, 
+/// и возвращающая результат int вычитания второго числа из первого. 
+/// Вызовите эту функцию в классе Main при помощи делегата и отобразите результат в консольном сообщении.
+/// </summary>
+namespace Delegate
+{
+    class Program
+    {
+        delegate int SubstractDelrgate(int a, int b);
+        static void Main(string[] args)
+        {
+            SubstractDelrgate substractDelrgate = Substract;
+            Console.WriteLine(substractDelrgate.Invoke(1, 2));
+
+        }
+        static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
