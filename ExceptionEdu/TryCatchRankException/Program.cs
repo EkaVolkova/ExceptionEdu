ca61c7f0d93d1caf36a8e6c563aa21576f20818b
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 17:06:54 +0500

    9.2.3 Создайте консольное решение, в котором реализуйте конструкцию Try/Catch/Finally для обработки исключения RankException. В случае исключения отобразите в консоль тип исключения (через метод GetType()).

---
+++ ExceptionEdu/TryCatchRankException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// 9.2.3Создайте консольное решение, в котором реализуйте конструкцию Try/Catch/Finally для обработки исключения RankException. 
+/// В случае исключения отобразите в консоль тип исключения (через метод GetType()).
+/// </summary>
+namespace TryCatchRankException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new RankException("Ошибка");
+            }
+            catch(RankException ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
