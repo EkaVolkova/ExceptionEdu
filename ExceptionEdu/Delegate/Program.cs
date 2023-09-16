149a74300d32681af48e918563b239ee468cec9c
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 17:40:32 +0500

    9.3.3 Используя ваше приложение из задания 9.3.2, реализуйте вызов делегата двумя разными способами.

--- ExceptionEdu/Delegate/Program.cs
+++ ExceptionEdu/Delegate/Program.cs
@@ -4,6 +4,8 @@
 /// 9.3.2 Создайте консольное приложение, в котором есть функция, принимающая на вход два числа int, 
 /// и возвращающая результат int вычитания второго числа из первого. 
 /// Вызовите эту функцию в классе Main при помощи делегата и отобразите результат в консольном сообщении.
+/// 
+/// 9.3.3 Используя ваше приложение из задания 9.3.2, реализуйте вызов делегата двумя разными способами.
 /// </summary>
 namespace Delegate
 {
@@ -14,6 +16,7 @@ namespace Delegate
         {
             SubstractDelrgate substractDelrgate = Substract;
             Console.WriteLine(substractDelrgate.Invoke(1, 2));
+            Console.WriteLine(substractDelrgate(1, 2));
 
         }
         static int Substract(int a, int b)
