2f59900ed14651a6ed2a81e68e38aa5b23791a91
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 18:48:25 +0500

    9.4.3 На основе скринкаста создайте консольное приложение, в котором реализуйте демонстрацию контравариантности делегатов при помощи следующей модели классов: class Parent {} class Child: Parent {}

---
+++ ExceptionEdu/Contravariance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// На основе скринкаста создайте консольное приложение, в котором реализуйте демонстрацию контравариантности делегатов при помощи следующей модели классов:
+/// class Parent { }
+/// class Child : Parent { }
+/// </summary>
+namespace Contravariance
+{
+    class Program
+    {
+        delegate void Handler(Child child);
+        static void Main(string[] args)
+        {
+            Handler handlerChild = HandlerChild;
+            Handler handlerParent = HandlerParent;
+        }
+        static void HandlerParent(Parent parent)
+        {
+            
+        }
+        static void HandlerChild(Child child)
+        {
+        }
+    }
+    class Parent { }
+    class Child : Parent { }
+}
