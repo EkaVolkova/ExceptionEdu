b691f830f3ad33748d5c2bd1adb27826e4b83f01
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 18:11:15 +0500

    9.3.12 Существует следующее консольное решение, которое отображает сообщение Hello World в консольном сообщении
    namespace DelegatePractices
    {
        class Program
        {
            delegate void ShowMessageDelegate(string _message);
            static void Main(string[] args)
            {
                ShowMessageDelegate showMessageDelegate = ShowMessage;
                showMessageDelegate.Invoke("Hello World!");
                Console.Read();
            }
    
            static void ShowMessage(string _message)
            {
                Console.WriteLine(_message);
            }
        }
    }
    Реализуйте в данном решении анонимный метод, не сломав логику приложения.

---
+++ ExceptionEdu/AnonymousDelegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>
+/// Существует следующее консольное решение, которое отображает сообщение Hello World в консольном сообщении:
+///     delegate void ShowMessageDelegate(string _message);
+///     static void Main(string[] args)
+///     {
+///         ShowMessageDelegate showMessageDelegate = ShowMessage;
+///         showMessageDelegate.Invoke("Hello World!");
+///         Console.Read();
+///     }
+///     static void ShowMessage(string _message)
+///     {
+///         Console.WriteLine(_message);
+///     }
+///     Реализуйте в данном решении анонимный метод, не сломав логику приложения.
+/// </summary>
+namespace AnonymousDelegates
+{
+    class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = delegate(string _message)
+            {
+                Console.WriteLine(_message);
+                
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
+        }
+
+    }
+}
